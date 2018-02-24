a202c313774f1943a66fb0da52392bca137366f8
kholysa <saif.elkholy@mail.mcgill.ca>
2018-02-23 22:57:13 -0500

    APDU validates correctly

--- src/main/java/residence/kholy/mobeewaveinterview/CommandAPDU.java
+++ src/main/java/residence/kholy/mobeewaveinterview/CommandAPDU.java
@@ -1,12 +1,101 @@
 package residence.kholy.mobeewaveinterview;
 
-public class CommandAPDU {
+/**
+ * ComandAPDU class contains fields for each of the headers of the CommandAPDU object
+ * Maximum size of 250 Bytes
+ * 4-header
+ * 0,1-Lc
+ * 243-Data
+ * 0,1,2,3-Le
+ */
+class CommandAPDU {
 
-    private byte[] header = new byte[4];
+    private byte[] header;
+    private byte[] lc;
+    private byte[] data;
+    private byte[] le;
+    private boolean valid = true;
 
-    public CommandAPDU(byte[] header){
-        this.header = header;
-//        return this;
+    CommandAPDU(byte[] header){
+        checkAndValidateHeader(header);
     }
 
+    CommandAPDU(byte[] header, byte[] le){
+        checkAndValidateLe(le);
+        checkAndValidateHeader(header);
+    }
+
+    CommandAPDU(byte[] header, byte[] lc, byte[] data){
+
+        checkAndValidateHeader(header);
+        checkAndValidateDataLength(lc, data);
+    }
+
+    CommandAPDU(byte[] header, byte[] lc, byte[] data, byte[] le){
+
+        checkAndValidateHeader(header);
+        checkAndValidateDataLength(lc,data);
+        checkAndValidateLe(le);
+    }
+
+    private void checkAndValidateHeader(byte[] header) {
+        byte headerLength = (byte) header.length;
+        if(headerLength != 4){
+            invalidateAPDUInstance();
+        }else
+            this.header = header;
+    }
+
+    private void checkAndValidateLe(byte[] le) {
+        byte leLength = (byte) le.length;
+        if (leLength > 3){
+            invalidateAPDUInstance();
+        } else
+            this.le = le;
+    }
+
+    private void checkAndValidateDataLength(byte[] lc, byte[] data) {
+        byte lcLength = (byte) lc.length;
+        short dataLength = (short) data.length;
+
+        if(lcLength == 0 && dataLength > 0){
+            invalidateAPDUInstance();
+            return;
+        } else if ( lcLength != 1 || dataLength > 243){
+            invalidateAPDUInstance();
+            return;
+        }
+        short expectedLengthOfData = (short) lc[0];
+        if (dataLength != expectedLengthOfData){
+            invalidateAPDUInstance();
+            return;
+        }
+
+        this.lc = lc;
+        this.data = data;
+
+    }
+    private void invalidateAPDUInstance() {
+        this.valid = false;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public byte[] getHeader() {
+        return header;
+    }
+
+    public byte[] getLc() {
+        return lc;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public byte[] getLe() {
+        return le;
+    }
 }
--- src/main/java/residence/kholy/mobeewaveinterview/MainActivity.java
+++ src/main/java/residence/kholy/mobeewaveinterview/MainActivity.java
@@ -9,22 +9,79 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView CommandAPDUTextOutput;
-    EditText CommandAPDUTextInput;
+    EditText CommandAPDUHeader;
+    EditText CommandAPDULc;
+    EditText CommandAPDUData;
+    EditText CommandAPDULe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CommandAPDUTextInput = (EditText) findViewById(R.id.CommandAPDUTextInput);
+        CommandAPDUHeader = (EditText) findViewById(R.id.commandADPUHeader);
+        CommandAPDULc = (EditText) findViewById(R.id.commandADPULc);
+        CommandAPDUData = (EditText) findViewById(R.id.commandADPUData);
+        CommandAPDULe = (EditText) findViewById(R.id.commandADPULe);
+
         CommandAPDUTextOutput = (TextView) findViewById(R.id.CommandAPDUTextOutput);
 
     }
 
     public void onAPDUButtonClick (View v) {
+        byte[] userAPDUHeader = hexStringToByteArray(CommandAPDUHeader.getText().toString());
+        byte[] userAPDULc = hexStringToByteArray(CommandAPDULc.getText().toString());
+        byte[] userAPDUData = hexStringToByteArray(CommandAPDUData.getText().toString());
+        byte[] userAPDULe = hexStringToByteArray(CommandAPDULe.getText().toString());
 
-        String userAPDUInput = CommandAPDUTextInput.getText().toString();
-        byte[] userAPDUInputAsByte = userAPDUInput.getBytes();
+        CommandAPDU commandAPDU = commandAPDUFactory(userAPDUHeader, userAPDULc, userAPDUData, userAPDULe);
 
-        CommandAPDUTextOutput.setText(userAPDUInputAsByte.toString());
+        String userAPDUInputAsString = "";
+        for (byte singleByte : userAPDUHeader) {
+            userAPDUInputAsString += ""+ singleByte;
+        }
+        CommandAPDUTextOutput.setText(userAPDUInputAsString);
+
+    }
+
+    /**
+     * Method which takes as input the hexadecimal string representation of an APDU section, and returns a byte array
+     * taken from https://stackoverflow.com/questions/140131/convert-a-string-representation-of-a-hex-dump-to-a-byte-array-using-java
+     * @param s APDU section as a hex representation
+     * @return byte array
+     */
+    private byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+
+
+    /**
+     * Factory for handling the different possibilities of APDU class
+     * @param header byte array header
+     * @param lc byte array lc
+     * @param data byte array data
+     * @param le byte array le
+     * @return validated CommandAPDU object
+     */
+    private CommandAPDU commandAPDUFactory(byte[] header, byte[] lc, byte[] data, byte[] le){
+        CommandAPDU commandAPDU;
+        if (header.length > 0 && lc.length + data.length + le.length == 0){
+            commandAPDU = new CommandAPDU(header);
+        } else if (header.length > 0 && le.length > 0 && lc.length + data.length == 0){
+            commandAPDU = new CommandAPDU(header, le);
+        } else if (header.length > 0 && lc.length > 0 && data.length > 0 && le.length == 0){
+            commandAPDU = new CommandAPDU(header, lc, data);
+        } else if (header.length > 0 && lc.length > 0 && data.length > 0 && le.length > 0){
+            commandAPDU = new CommandAPDU(header, lc, data, le);
+        } else {
+            byte[] invalidateByteArray = {};
+            commandAPDU = new CommandAPDU(invalidateByteArray);
+        }
+        return commandAPDU;
     }
 }
